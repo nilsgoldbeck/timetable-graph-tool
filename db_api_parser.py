a4f8676a7ef665e4dd8bfb4a9b819cc7b88fc677
Nils Goldbeck <n.goldbeck14@imperial.ac.uk>
2019-06-17 00:42:54 +0100

    Further testing and improvement

--- db_api_parser.py
+++ db_api_parser.py
@@ -1,4 +1,5 @@
-import datetime
+import datetime as dt
+from time import sleep
 import json
 import requests
 import urllib.parse
@@ -6,17 +7,203 @@ from timetable import timetable
 
 class db_api_parser():
 
+    url_stations = "https://api.deutschebahn.com/stada/v2/stations"
+    url_departures = "https://api.deutschebahn.com/fahrplan-plus/v1/departureBoard/"
+    url_journey_details = "https://api.deutschebahn.com/fahrplan-plus/v1/journeyDetails/"
+
+    max_wait_time = 1
+    time_increment = 240
+
     def __init__(self, api_key):
         self.api_key = api_key
 
+        self.departure_request_times = []
+        self.trip_request_times = []
 
-    def get_timetable(self, begin, end, stop_categories, trip_categories):
+    def get_timetable(self, start, end, stop_categories = '1-7'):
 
-        stops = {}
+        self.header = self.get_header()
+
+        eva_numbers = self.get_eva_numbers(stop_categories)
+
+        trips = self.get_trips(start, end, eva_numbers)
+
+        return timetable(start, end, trips)
+
+
+    def get_header(self):
+
+        return { "Accept": "application/json", "Authorization": "Bearer " + self.api_key }
+
+
+    def get_eva_numbers(self, categories):
+
+        data = {'category': categories}
+
+        response = requests.get(self.url_stations, data, headers=self.header)
+
+        eva_numbers = []
+
+        for station in response.json()['result']:
+            for number in station['evaNumbers']:
+                eva_numbers.append(number['number'])
+
+        return eva_numbers
+
+
+    def get_departures(self, eva_number, datetime):
+
+        url = self.url_departures + str(eva_number)
+        data = {'date': datetime.isoformat()}
+
+        try:
+            self.count_dep_requests += 1
+            response = requests.get(url, data, headers=self.header, timeout=self.max_wait_time)
+        except (requests.Timeout):
+            self.count_dep_requests_timeout += 1
+            return
+        except (requests.ConnectionError):
+            self.count_dep_requests_failed += 1
+            return
+
+        #self.departure_request_times.append(response.elapsed.total_seconds())
+
+        if response.status_code is not 200:
+            self.count_dep_requests_failed += 1
+            return
+
+        departures = []
+
+        for departure in response.json():
+
+            dep_time = dt.datetime.strptime(departure['dateTime'], "%Y-%m-%dT%H:%M")
+            
+            departures.append({'trip_id': departure['name'], 'loc_id': eva_number, 'dep_time': dep_time, 'details_id': departure['detailsId']})
+
+        return departures
+
+
+    def get_journey_details(self, departure):
+
+        url = self.url_journey_details + urllib.parse.quote(departure['details_id'])
+
+        try:
+            self.count_trip_requests += 1
+            response = requests.get(url, headers=self.header, timeout=self.max_wait_time)
+        except (requests.Timeout):
+            self.count_trip_requests_timeout += 1
+            return
+        except (requests.ConnectionError):
+            self.count_trip_requests_failed += 1
+            return
+
+        #self.trip_request_times.append(response.elapsed.total_seconds())
+
+        if response.status_code is not 200:
+            self.count_trip_requests_failed += 1
+            return
+
+        stops = response.json()
+
+        if stops[0]['stopId'] == departure['loc_id']:
+
+            trip = []
+
+            current_datetime = departure['dep_time']
+
+            for stop in stops:
+
+                if 'arrTime' in stop:
+                    arr_time = dt.datetime.strptime(stop['arrTime'], '%H:%M').time()
+                    arr_time = dt.datetime.combine(current_datetime.date(), arr_time)
+                    if arr_time < current_datetime:
+                        arr_time = arr_time + dt.timedelta(days=1)
+                    current_datetime = arr_time
+                    arr_time = arr_time.isoformat()
+                else:
+                    arr_time = None
+
+                if 'depTime' in stop:
+                    dep_time = dt.datetime.strptime(stop['depTime'], '%H:%M').time()
+                    dep_time = dt.datetime.combine(current_datetime.date(), dep_time)
+                    if dep_time < current_datetime:
+                        dep_time = dep_time + dt.timedelta(days=1)
+                    current_datetime = dep_time
+                    dep_time = dep_time.isoformat()
+                else:
+                    dep_time = None      
+
+                trip.append({'loc_id': stop['stopId'], 'loc_name': stop['stopName'], 'dep_time': dep_time, 'arr_time': arr_time,
+                    'lat': stop['lat'], 'lon': stop['lon'], 'trip_id': stop['train'], 'trip_type': stop['type']})
+
+            return trip
+
+
+    def get_trips(self, start, end, eva_numbers):
+
         trips = {}
 
-        #TODO get stops
+        self.count_dep_requests = 0
+        self.count_dep_requests_timeout = 0
+        self.count_dep_requests_failed = 0
+        self.count_trip_requests = 0
+        self.count_trip_requests_timeout = 0
+        self.count_trip_requests_failed = 0
+        self.count_evas_complete = 0
 
-        #TODO get trips
+        for eva_number in eva_numbers:
 
-        return timetable(begin, stops, trips)
+            current_time = start
+
+            while current_time < end:
+
+                print('Gathering trips from {} at {}. Completed {} of {} stops. Gathered {} trips.'.format(
+                    eva_number, current_time, self.count_evas_complete, len(eva_numbers), len(trips)), end='\r')
+
+                departures = self.get_departures(eva_number, current_time)
+
+                if departures is None:
+                    current_time += dt.timedelta(minutes=self.time_increment)
+                    continue
+
+                for departure in departures:
+                    
+                    trip_id = departure['trip_id']
+
+                    if trip_id not in trips:
+                        trips[trip_id] = []
+
+                    trip = self.get_journey_details(departure)
+
+                    if trip is not None:
+                        trips[trip_id].append(trip)
+
+                if len(departures) > 0:
+                    current_time = departures[-1]['dep_time'] + dt.timedelta(minutes=1)
+                else:
+                    current_time = current_time + dt.timedelta(minutes=self.time_increment)
+
+            self.count_evas_complete += 1
+
+        print('\n')
+        print('Departure board API:')
+        print('Requests: {}'.format(self.count_dep_requests))
+        print('Timeout: {}'.format(self.count_dep_requests_timeout))
+        print('Failed: {}'.format(self.count_dep_requests_failed))
+
+        print('\n')
+        print('Journey details API:')
+        print('Requests: {}'.format(self.count_trip_requests))
+        print('Timeout: {}'.format(self.count_trip_requests_timeout))
+        print('Failed: {}'.format(self.count_trip_requests_failed))
+
+        print('\n')
+        print('{} trips gathered'.format(len(trips)))
+
+        return trips
+
+
+    def save_trips_to_file(self, trips, filename):
+
+        with open(filename, 'w') as f:
+            json.dump(trips, f)
--- timetable.py
+++ timetable.py
@@ -1,48 +1,301 @@
+import json
+import datetime
+from difflib import get_close_matches
+from geopy import distance
 from graph_tool.all import *
 
 class timetable:
 
-    def __init__(self, time_zero, stops, trips):
-        self.time_zero = time_zero
-        self.stops = stops
+    def __init__(self, begin, end, trips = {}):
+        self.begin = begin
+        self.end = end
         self.trips = trips
 
+    def load_trips_from_file(self, filename):
 
-    def generate_graph(self, min_transfer_time, max_transfer_time):
+        with open(filename, 'r') as f:
+            self.trips = json.load(f)
+
+
+    def get_locations_from_trips(self, trips, max_transfer_distance):
+
+        locations = {}
+
+        print('Generating locations from trips')
+
+        for trip_name, trip_list in trips.items():
+
+            print('Adding locations for {}'.format(trip_name + ' '*50), end='\r')
+
+            for trip in trip_list:
+
+                for stop in trip:
+
+                    eva_number = stop['loc_id']
+
+                    if eva_number not in locations:
+
+                        new_location = {'name': stop['loc_name'], 
+                            'lat': stop['lat'], 'lon': stop['lon'],
+                            'within_transfer_distance': []}
+
+                        for existing_id, existing_location in locations.items():
+
+                            p1 = (float(existing_location['lat']), float(existing_location['lon']))
+                            p2 = (float(new_location['lat']), float(new_location['lon']))
+                            d = distance.distance(p1, p2).meters
+
+                            if d < max_transfer_distance:
+                                new_location['within_transfer_distance'].append(existing_id)
+
+                        for neighbour_id in new_location['within_transfer_distance']:
+                            locations[neighbour_id]['within_transfer_distance'].append(eva_number)
+
+                        locations[eva_number] = new_location
+
+        return locations
+
+
+    def add_transfer_edges(self, min_transfer_time, max_transfer_time):
+
+        print('\nGenerating transfer links (transfer time {} to {} minutes)'.format(
+            min_transfer_time, max_transfer_time))
+
+        for loc_id, loc in self.locations.items():
+
+            print('Adding transfers at {}'.format(loc['name'] + ' '*50), end='\r')
+
+            for arr_time, arr_vertice in self.arrival_vertices[loc_id].items():
+                for conn_loc_id in ([loc_id] + loc['within_transfer_distance']):
+                    for dep_time, dep_vertice in self.departure_vertices[conn_loc_id].items():
+
+                        if dep_time > arr_time + min_transfer_time:
+                            if dep_time < arr_time + max_transfer_time:
+
+                                self.add_edge(arr_vertice, dep_vertice, False, False, True)
+
+
+
+    def generate_graph(self, max_transfer_distance, min_transfer_time, max_transfer_time):
 
         self.g = self.initialize_graph()
 
-        #TODO Add trips
+        self.locations = self.get_locations_from_trips(self.trips, max_transfer_distance)
 
-        #TODO Add transfers
+        self.departure_vertices = {}
+        self.arrival_vertices = {}
 
-        pass
+        for loc_id, loc in self.locations.items():
+            self.departure_vertices[loc_id] = {}
+            self.arrival_vertices[loc_id] = {}
+
+        # Add trips:
+
+        print('\nGenerating nodes and vertices from trips')
+
+        for trip_name, trip_list in self.trips.items():
+
+            for trip in trip_list:
+
+                origin = trip[0]
+                destination = trip[-1]
+
+                print('Adding nodes and vertices for {}      '.format(trip_name), end='\r')
+
+                # Add origin:
+
+                dep_vertice = self.add_vertice(origin['loc_id'], origin['loc_name'],
+                    self.get_timestamp(origin['dep_time']),
+                    origin['lat'], origin['lon'], is_departure=True)
+
+                # Add stops:
+
+                for i in range(1, len(trip) - 1):
+
+                    stop = trip[i]  
+
+                    arr_vertice = self.add_vertice(stop['loc_id'], stop['loc_name'],
+                        self.get_timestamp(stop['arr_time']),
+                        stop['lat'], stop['lon'], is_departure=False)
+
+                    self.add_edge(dep_vertice, arr_vertice, True, False, False,
+                        stop['trip_id'], stop['trip_type'], origin['loc_id'], origin['loc_name'],
+                        destination['loc_id'], destination['loc_name'])
+
+                    dep_vertice = self.add_vertice(stop['loc_id'], stop['loc_name'],
+                        self.get_timestamp(stop['dep_time']),
+                        stop['lat'], stop['lon'], is_departure=True)
+
+                    self.add_edge(arr_vertice, dep_vertice, True, True, False,
+                        stop['trip_id'], stop['trip_type'], origin['loc_id'], origin['loc_name'],
+                        destination['loc_id'], destination['loc_name'])
+
+                # Add destination:
+                
+                arr_vertice = self.add_vertice(destination['loc_id'], destination['loc_name'],
+                    self.get_timestamp(destination['arr_time']),
+                    destination['lat'], destination['lon'], is_departure=False)
+
+                self.add_edge(dep_vertice, arr_vertice, True, False, False,
+                        stop['trip_id'], stop['trip_type'], origin['loc_id'], origin['loc_name'],
+                        destination['loc_id'], destination['loc_name'])
+
+
+        # Add transfers:
+
+        self.add_transfer_edges(min_transfer_time, max_transfer_time)
+
+        return self.g
 
 
     def initialize_graph(self):
 
         g = Graph()
 
-        #TODO Add vertex properties
-        #station-name <string>
-        #eva <short>
-        #departure <bool>
-        #arrival <bool>
-        #time <short>
-        #pos vector<double>
-        #lat <double>
-        #lon <double>
+        g.vp.loc_id = g.new_vertex_property('int')
+        g.vp.loc_name = g.new_vertex_property('string')
+        g.vp.time = g.new_vertex_property('int')
+        g.vp.pos = g.new_vertex_property('vector<double>')
+        g.vp.lat = g.new_vertex_property('double')
+        g.vp.lon = g.new_vertex_property('double')
+        g.vp.is_departure = g.new_vertex_property('bool')
 
-        #TODO Add edge properties
-        #length <short>
-        #transfer <bool>
-        #train-stationary <bool>
-        #train-travelling <bool>
-        #train-id <string>
-        #train-type <string>
-        #train-origin-name <string>
-        #train-origin-eva <short>
-        #train-destination-name <strin>
-        #train-destination-eva <string>
+        g.ep.duration = g.new_edge_property('int')
+        g.ep.is_transport = g.new_edge_property('bool')
+        g.ep.is_stationary = g.new_edge_property('bool')
+        g.ep.is_transfer = g.new_edge_property('bool')
+        g.ep.transport_id = g.new_edge_property('string')
+        g.ep.transport_type = g.new_edge_property('string')
+        g.ep.transport_origin_id = g.new_edge_property('string')
+        g.ep.transport_origin_name = g.new_edge_property('string')
+        g.ep.transport_destination_id = g.new_edge_property('string')
+        g.ep.transport_destination_name = g.new_edge_property('string')
 
         return g
+
+
+    def add_vertice(self, loc_id, loc_name, time, lat, lon, is_departure=True):
+
+        v = None
+
+        if is_departure:
+            if time in self.departure_vertices[loc_id]:
+                return self.departure_vertices[loc_id][time]
+        else:
+            if time in self.arrival_vertices[loc_id]:
+                return self.arrival_vertices[loc_id][time]
+
+
+        v = self.g.add_vertex()
+
+        self.g.vp.loc_id[v] = loc_id
+        self.g.vp.loc_name[v] = loc_name
+        self.g.vp.time[v] = time
+        self.g.vp.pos[v] = (lon, lat)
+        self.g.vp.lat[v] = lat
+        self.g.vp.lon[v] = lon
+        self.g.vp.is_departure[v] = is_departure
+
+        if is_departure:
+            self.departure_vertices[loc_id][time] = v
+        else:
+            self.arrival_vertices[loc_id][time] = v
+
+        return v
+
+
+    def add_edge(self, v1, v2, is_transport, is_stationary, is_transfer, transport_id = None, transport_type = None, origin_id = None,
+        origin_name = None, destination_id = None, destination_name = None):
+
+        e = self.g.add_edge(v1, v2)
+
+        self.g.ep.duration[e] = self.g.vp.time[v2] - self.g.vp.time[v1]
+        self.g.ep.is_transport[e] = is_transport
+        self.g.ep.is_stationary[e] = is_stationary
+        self.g.ep.is_transfer[e] = is_transfer
+        self.g.ep.transport_id[e] = transport_id
+        self.g.ep.transport_type[e] = transport_type
+        self.g.ep.transport_origin_id[e] = origin_id
+        self.g.ep.transport_origin_name[e] = origin_name
+        self.g.ep.transport_destination_id[e] = destination_id
+        self.g.ep.transport_destination_name[e] = destination_name
+
+        return e
+
+
+    def get_timestamp(self, time_str):
+
+        time = datetime.datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S')
+
+        timestamp = int((time - self.begin).total_seconds() / 60)
+
+        return timestamp
+
+
+    def print_summary(self):
+
+        print('Locations: {}'.format(len(self.locations)))
+        print('Vertices: {}'.format(self.g.num_vertices()))
+        print('Edges: {}'.format(self.g.num_edges()))
+
+        self.g.set_edge_filter(self.g.ep.is_transport)
+        print('On-tranport edges: {}'.format(self.g.num_edges()))
+
+        self.g.set_edge_filter(self.g.ep.is_stationary)
+        print('Stationary on-tranport edges: {}'.format(self.g.num_edges()))
+
+        self.g.set_edge_filter(self.g.ep.is_transfer)
+        print('Transfer edges: {}'.format(self.g.num_edges()))
+
+        self.g.clear_filters()
+
+
+    def search_location(self, search_string):
+
+        loc_names = [v['name'] for k, v in self.locations.items()]
+
+        result =  get_close_matches(search_string, loc_names, n=1)
+
+        loc_name_found = result[0]
+
+        loc_id_found = next((x[0] for x in self.locations.items() if x[1]['name'] == loc_name_found), None)
+
+        return loc_name_found, loc_id_found
+
+
+    def find_route(self, from_loc_id, to_loc_id, dep_time):
+
+        dep_timestamp = self.get_timestamp(dep_time.isoformat())
+
+        origin_vertice = self.g.add_vertex()
+        destination_vertice = self.g.add_vertex()
+
+        boarding_edges = []
+
+        for t, v in self.departure_vertices[from_loc_id].items():
+            if t > dep_timestamp:
+                #e = self.add_edge(origin_vertice, v, False, False, False)
+                e = self.g.add_edge(origin_vertice, v)
+                self.g.ep.duration[e] = t - dep_timestamp
+                boarding_edges.append(e)
+
+        
+        alighting_edges = []
+
+        for t, v in self.arrival_vertices[to_loc_id].items():
+            #e = self.add_edge(v, destination_vertice, False, False, False)
+            e = self.g.add_edge(v, destination_vertice)
+            self.g.ep.duration[e] = 0
+            alighting_edges.append(e)
+
+        
+        shortest_paths = graph_tool.topology.all_shortest_paths(self.g, origin_vertice,
+            destination_vertice, weights=self.g.ep.duration)
+
+        for e in (boarding_edges + alighting_edges):
+            self.g.remove_edge(e)
+
+        #self.g.remove_vertex(origin_vertice)
+        #self.g.remove_vertex(destination_vertice)
+
+        return shortest_paths
